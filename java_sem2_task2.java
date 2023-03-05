314893f62e3c213cffb7567c4b32550f5946ae9e
Eliseev Denis <medved-ed@mail.ru>
2023-03-05 13:38:26 +0800

    sem2 task2 add log

--- java_sem2_task2.java
+++ java_sem2_task2.java
@@ -1,4 +1,8 @@
+
 import java.util.Arrays;
+import java.io.IOException;
+import java.util.logging.*;
+
 
 /**
  * 2.Реализуйте алгоритм сортировки пузырьком числового массива, 
@@ -6,23 +10,34 @@ import java.util.Arrays;
  */
 
 public class java_sem2_task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        
         int [] array = new int[] {5, 4, 3, 2, 0, 3, 56, 8};
-        System.out.println(Arrays.toString(bubleSort(array)));
+        //System.out.println(Arrays.toString(bubleSort(array)));
     }
-    public static int[] bubleSort (int[] arr) {
+
+    public static int[] bubleSort (int[] arr) throws IOException   {
         int temp;
+        Logger logger = Logger.getLogger (java_sem2_task2.class.getName());
+        FileHandler fh = new FileHandler("log.txt");
+        logger.addHandler (fh);
+        SimpleFormatter sf = new SimpleFormatter();
+        fh.setFormatter(sf);
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length-1; j++) {
                 if (arr[j]>arr[j+1]) {
                     temp = arr[j+1];
                     arr[j+1] = arr[j];
                     arr[j]=temp;
+                    String strarr = Arrays.toString(arr);
+                    logger.log(Level.INFO, strarr);
                 }
             }
         }
         return arr;
     }
+
+    
         
 }
 
